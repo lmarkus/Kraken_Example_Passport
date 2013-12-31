595989c1500bf339c739a2fa1ecab1d7eb07fb8e
lmarkus <lmarkus@paypal.com>
2013-12-30 22:19:21 -0800

    Require authentication for admin and profile pages

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,8 @@
 'use strict';
 
 
-var AdminModel = require('../models/admin');
+var AdminModel = require('../models/admin'),
+    auth = require('../lib/auth');
 
 
 module.exports = function (app) {
@@ -9,10 +10,10 @@ module.exports = function (app) {
     var model = new AdminModel();
 
 
-    app.get('/admin', function (req, res) {
-        
+    app.get('/admin', auth.isAuthenticated('admin'), function (req, res) {
+
         res.render('admin', model);
-        
+
     });
 
 };
--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,8 @@
 'use strict';
 
 
-var ProfileModel = require('../models/profile');
+var ProfileModel = require('../models/profile'),
+    auth = require('../lib/auth');
 
 
 module.exports = function (app) {
@@ -9,10 +10,10 @@ module.exports = function (app) {
     var model = new ProfileModel();
 
 
-    app.get('/profile', function (req, res) {
-        
+    app.get('/profile', auth.isAuthenticated(), function (req, res) {
+
         res.render('profile', model);
-        
+
     });
 
 };
